d7416fa507c23ce7356148497364a750eecbea42
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-08-02 17:32:51 +0530

    Added UUID mechanism

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {v4 as uuidV4} from 'uuid';
 
 const HomePage = () => {
+
+    const [roomId, setRoomId] = useState('');
+    const [userName, setUserName] = useState('');
+
+    const createNewRoom = (e) =>{
+
+        e.preventDefault(); //prevent page from re-loading as this is in anchor tag
+
+        const id = uuidV4;
+        setRoomId(id);
+        console.log(id);
+
+    }
+
   return (
     <div className='homePageWrapper'>
         <div className='formWrapper'>
@@ -11,16 +26,20 @@ const HomePage = () => {
                     type='text'
                     className='inputBox'
                     placeholder='ROOM ID'
+                    onChange={(e) => setRoomId(e.target.value)}
+                    value={roomId}
                 />
                 <input 
                     type='text'
                     className='inputBox'
                     placeholder='USERNAME'
+                    onChange={(e) => setUserName(e.target.value)}
+                    value={userName}
                 />
                 <button className='btn joinBtn'>Join</button>
                 <span className="createInfo">
                     If you don't have an invite then create &nbsp; 
-                    <a href="" className='createNewBtn'>
+                    <a onClick={createNewRoom} href="" className='createNewBtn'>
                         new room
                     </a>
                 </span>

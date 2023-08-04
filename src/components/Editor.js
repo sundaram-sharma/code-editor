a50c6935c3838d7898f38c1957a886363492b58f
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-08-05 01:42:19 +0530

    Editor design completed

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import Codemirror from 'codemirror';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/lib/codemirror.css';
 
 const Editor = () => {
+
+  useEffect(()=>{
+    async function init(){
+
+
+      console.log("Exert")
+
+      Codemirror.fromTextArea(document.getElementById('realtimeEditor'),{
+        mode:{name: 'javascript', json: true},
+        theme: 'dracula',
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true
+
+      });
+
+    }
+    init();
+  },[]);
+
   return (
-    <div>Editor</div>
+    <textarea id="realtimeEditor">
+
+    </textarea>
   )
 }
 
